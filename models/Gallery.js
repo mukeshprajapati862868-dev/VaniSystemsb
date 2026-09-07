ddc85cf1d2ab3041135384c211f758ef0d8e8f4c
mukeshprajapati862868-dev <95376333+mukeshprajapati862868-dev@users.noreply.github.com>
2026-09-07 15:01:53 +0530

    Update Gallery.js

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,23 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const GallerySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  filename: {
-    type: String,
-    required: true
-  },
-  path: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+const GallerySchema =
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
 
-module.exports = mongoose.model('Gallery', GallerySchema);
+      filename: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+
+      path: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    }
+  );
+
+module.exports =
+  mongoose.model(
+    "Gallery",
+    GallerySchema
+  );
