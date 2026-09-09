52b83061a851f0f8bb1c0ec02e378e8684ea5677
mukeshprajapati862868-dev <95376333+mukeshprajapati862868-dev@users.noreply.github.com>
2026-09-09 17:27:56 +0530

    Refactor Gallery schema formatting

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,32 +1,32 @@
 const mongoose = require("mongoose");
 
-const GallerySchema =
-  new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-        trim: true,
-      },
+const GallerySchema = new mongoose.Schema({
 
-      filename: {
-        type: String,
-        required: true,
-        trim: true,
-      },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
 
-      path: {
-        type: String,
-        required: true,
-        trim: true,
-      },
+  filename: {
+    type: String,
+    required: true,
+    trim: true
+  },
 
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-    }
-  );
+  path: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
 
 module.exports =
   mongoose.model(
